a78aa379fa77ecfa626f220913290f1ce0eb4759
Nayely Perez <104110000+nayely18@users.noreply.github.com>
2022-12-11 19:31:24 -0800

    Update IFaqService.cs

--- dotnet/IFaqService.cs
+++ dotnet/IFaqService.cs
@@ -1,1 +1,15 @@
+using MiVet.Models.Domain.FAQ;
+using MiVet.Models.Requests.FAQ;
+using System.Collections.Generic;
 
+namespace MiVet.Services.Interfaces
+{
+    public interface IFAQService
+    {
+        int Add(FAQAddRequest model);
+        void Delete(int id);
+        List<FAQ> Get(int CategoryId);
+        List<FAQ> GetAll();
+        void Update(FAQUpdateRequest model);
+    }
+}
