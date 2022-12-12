794ee1e529962bdcad52558701835575e9702f57
Nayely Perez <104110000+nayely18@users.noreply.github.com>
2022-12-11 19:20:49 -0800

    Create Faq.cs

---
+++ Faq.cs
@@ -0,0 +1,22 @@
+using MiVet.Models.Domain.faq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiVet.Models.Domain.FAQ
+{
+    public class FAQ
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public FAQCategory Category { get; set; }
+        public int SortOrder { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+    }
+}
